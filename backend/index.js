81958ae1406356aff9c52499199009dee13d4d76
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-12 19:54:20 -0300

    Update book route.

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import { Book } from "./models/bookModel.js";
 const app = express();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
@@ -41,32 +41,59 @@ app.post("/books", async (request, response) => {
 
 // Route for get all Books
 app.get("/books", async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({ message: error.message })
-    }
-})
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 // Route for get one Book by id
 app.get("/books/:id", async (request, response) => {
   try {
-      const { id } = request.params;
+    const { id } = request.params;
 
-      const book = await Book.findById(id);
-      return response.status(200).json(book);
-
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
   } catch (error) {
-      console.log(error.message)
-      response.status(500).send({ message: error.message })
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
   }
-})
+});
+
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body, {
+      new: true,
+    });
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBUrl)
