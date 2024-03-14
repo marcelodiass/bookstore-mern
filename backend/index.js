8cd6b1d46f638c9c56b615ed2964243ac0d4fd03
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-14 17:51:06 -0300

    add route for delete book

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,7 @@ app.get("/books/:id", async (request, response) => {
   }
 });
 
+// Route for update a Book
 app.put("/books/:id", async (request, response) => {
   try {
     if (
@@ -94,6 +95,24 @@ app.put("/books/:id", async (request, response) => {
     response.status(500).send({ message: error.message });
   }
 });
+
+// Route for Delete a Book
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params
+    const result = await Book.findByIdAndDelete(id)
+
+    if (!result) {
+      return response.status(404).json({message: "Book not found"})
+    }
+
+    return response.status(200).send({message: "Book deleted successfully"})
+
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({ message: error.message })
+  }
+})
 
 mongoose
   .connect(mongoDBUrl)
